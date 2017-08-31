2d288230a787c15705a630f34f7ccd99d88c4018
Evgen <eok8177@gmail.com>
2017-08-31 14:58:35 +0300

    Pages: show

--- resources/views/frontend/pages/show.blade.php
+++ resources/views/frontend/pages/show.blade.php
@@ -1,7 +1,23 @@
 @extends('frontend.layouts.default')
 
 @section('content')
-    <div class="container">
-        <h1>{{ $page->title }}</h1>
-    </div>
+	<div class="container pages">
+		<div class="row">
+			<div class="col-md-8">
+				<h1 class="title">{{ $page->title }}</h1>
+				<div class="text">{!! $page->content !!}</div>
+			</div>
+			<div class="col-md-4 hidden-xs">
+				<ul class="menu-pages">
+					<li><a href="#" class="active">Про проект</a></li>
+					<li><a href="#">Допомога</a></li>
+					<li><a href="#">Правила </a></li>
+					<li><a href="#">Умови та конфіденційність</a></li>
+					<li><a href="#">Зворотний зв'язок</a></li>
+					<li><a href="#">Карта сайту</a></li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
 @endsection
