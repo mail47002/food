d29fae00418d62ac4d07a559a4ff644a57da64de
Evgen <eok8177@gmail.com>
2017-09-15 22:07:43 +0300

    Profile reviews

--- resources/views/frontend/profile/reviews.blade.php
+++ resources/views/frontend/profile/reviews.blade.php
@@ -35,7 +35,124 @@
 		</div>
 
 		<div class="col-md-9">
+			<h5 class="text-upper underline-red">Мої відгуки</h5><hr class="zerro-top">
+
+			<div class="filter-block">
+				<ul class="categories list-inline text-center">
+					<li><a href="#" class="link-red text-upper active">Відгуки про поварів (20)</a></li>
+					<li><a href="#" class="link-red text-upper">Відгуки про клієнтів (10)</a></li>
+				</ul>
+				<hr class="red-border">
+			</div>
+
 			<div class="v-indent-20"></div>
+
+						<div class="reviews">
+							<h5 class="text-upper underline-red">Відгуки (30)</h5><hr class="zerro-top">
+							<ul class="list-unstyled">
+			<!--    -->
+								<li class="with-image bg-yellow clearfix">
+									<div class="title">
+										<p class="date">2 липня 2016</p>
+										<p class="black">Відгук для <a href="#" class="link-blue">Оксана</a> про <a href="#" class="link-blue">М'ясне рагу з овочами</a></p>
+									</div>
+									<div class="left">
+										<div class="avatar">
+											<div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div>
+										</div>
+										<a href="#" class="link-blue name">Вікторія</a>
+									</div>
+
+									<div class="right">
+										<div class="date">2 липня 2016</div>
+										<span class="stars">4</span>
+										<div class="message">
+											В принципе вкусно,если сделать для одного раза,а так: гарнир (рис с изюмом, инжиром, морковь и луком) всетаки сладкий,много не съешь,а индейка суховат.
+										</div>
+
+										<div class="answer clearfix">
+											<div class="title">Ваша відповідь</div>
+											<div class="message">
+												В принципе вкусно,если сделать для одного раза,а так: гарнир
+											</div>
+											<div class="right-avatar">
+												<div class="avatar">
+													<div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div>
+												</div>
+											</div>
+										</div>
+
+										<div class="message">
+											В принципе вкусно,если сделать для одного раза,а так: гарнир (рис с изюмом, инжиром, морковь и луком) всетаки сладкий,много не съешь,а индейка суховат.
+										</div>
+
+										<hr>
+										<a href="#" class="link-blue pull-right">Приховати</a>
+
+									</div>
+									<div class="image">
+										<img src="/uploads/food1.jpg" alt="foto">
+									</div>
+								</li>
+			<!--    -->
+								<li class="with-image bg-yellow clearfix">
+									<div class="title">
+										<p class="date">2 липня 2016</p>
+										<p class="black">Відгук про клієнта <a href="#" class="link-blue">Оксана</a></p>
+									</div>
+									<div class="left">
+										{{-- <div class="avatar">
+											<div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div>
+										</div>
+										<a href="#" class="link-blue name">Вікторія</a> --}}
+									</div>
+
+									<div class="right">
+										<div class="date">2 липня 2016</div>
+										<span class="stars">4</span>
+										<div class="message">
+											В принципе вкусно,если сделать для одного раза,а так: гарнир (рис с изюмом, инжиром, морковь и луком) всетаки сладкий,много не съешь,а индейка суховат.
+										</div>
+
+										<div class="answer clearfix">
+											<div class="title">Ваша відповідь</div>
+											<div class="message">
+												В принципе вкусно,если сделать для одного раза,а так: гарнир
+											</div>
+											<div class="right-avatar">
+												<div class="avatar">
+													<div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div>
+												</div>
+											</div>
+										</div>
+
+										<div class="message">
+											В принципе вкусно,если сделать для одного раза,а так: гарнир (рис с изюмом, инжиром, морковь и луком) всетаки сладкий,много не съешь,а индейка суховат.
+										</div>
+
+										<hr>
+										<a href="#" class="link-blue pull-right">Приховати</a>
+
+									</div>
+									<div class="image">
+										<div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div>
+									</div>
+								</li>
+
+						</ul>
+					</div>
+
+
+					<div class="paginate">
+						<ul class="pagination grey">
+							<li><a href="#" rel="prev"><</a></li>
+							<li class="active"><a href="#">1</a></li>
+							<li><a href="#">2</a></li>
+							<li><a href="#" rel="next">></a></li>
+						</ul>
+					</div>
+
+
 	</div>
 </div>
 
