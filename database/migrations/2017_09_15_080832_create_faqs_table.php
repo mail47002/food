070f4c36c4b56932f0f32231c4468d6516ac34a3
Roman K <modern.diz@gmail.com>
2017-09-21 17:26:30 +0300

    Add FAQs

---
+++ database/migrations/2017_09_15_080832_create_faqs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFaqsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faqs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->integer('status')->default(1);
+            $table->integer('sort_order')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('faqs');
+    }
+}
