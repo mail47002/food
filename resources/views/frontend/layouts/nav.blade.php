1bffa733c0793c50a0b46b830b062b0ec7fff469
Evgen <eok8177@gmail.com>
2017-09-23 23:05:34 +0300

    User: 4.2.1, 4.3.1, 4.4.1 products, adverts, articles

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -9,9 +9,12 @@
 				<li><a href="{{ url('umovi-ta-konfidentsiynist') }}">Уммови та конфіденційність</a></li>
 				<li><a href="/contact">Зворотній зв'язок</a></li>
 				<li><a href="#">Карта сайту</a></li>
-<br>
-				<li class="active"><a href="/temp/user.index">user_page (4.1)</a></li
-<br>
+<hr>
+				<li class="active"><a href="/temp/user.index">user_page (4.1)</a></li>
+				<li class="active"><a href="/temp/user.products">user_page_products (4.2)</a></li>
+				<li class="active"><a href="/temp/user.adverts">user_page_adverts (4.3)</a></li>
+				<li class="active"><a href="/temp/user.articles">user_page_articles (4.4)</a></li>
+<hr>
 				<li class="active"><a href="/adverts">Об'яви (2)</a></li>
 				<li class="active"><a href="/products">Блюда (12)</a></li>
 				<li class="active"><a href="/temp/temp.add_food">add_food (7.1)</a></li>
