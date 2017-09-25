0527573cf019c37987018ce1d09180dfb0b175b8
Evgen <eok8177@gmail.com>
2017-09-25 21:36:24 +0300

    fix: Profile products

--- resources/views/frontend/profile/products.blade.php
+++ resources/views/frontend/profile/products.blade.php
@@ -37,6 +37,7 @@
 		<div class="col-md-9">
 			<h5 class="text-upper underline-red">Каталог страв ({{count($products)}})</h5><hr class="zerro-top">
 			<a href="#" class="button button-red button-big">Додати страву до каталогу</a>
+			<div class="v-indent-30"></div>
 
 			<hr>
 			<div class="bg-yellow">
@@ -92,21 +93,8 @@
 					</div>
 				</div>
 			</div>
-<<<<<<< HEAD
 			@endforeach
-=======
-@endfor
 
-
-{{-- Если пусто, выводить этот блок --}}
-			<div class="v-indent-40"></div>
-			<div class="empty-block">
-				<i class="fo fo-dish-search fo-2x"></i> 
-				<p class="text">У вас немає страв</p>
-				<a href="#" class="button button-red button-big">Додати страву до меню</a>
-			</div>
-
->>>>>>> 5b03a3da1d899bfb82eb81b0ffee09c3c42c7c1a
 			<div class="paginate">
 				<ul class="pagination grey">
 					<li><a href="#" rel="prev"><</a></li>
@@ -116,6 +104,7 @@
 				</ul>
 			</div>
 			@else
+			<div class="v-indent-40"></div>
 			<div class="empty-block">
 				<i class="fo fo-dish-search fo-2x"></i>
 				<p class="text">У вас немає страв</p>
