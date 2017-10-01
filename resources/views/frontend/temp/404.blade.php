1103011b29cd636b9aba0338c147905f6533f349
Evgen <eok8177@gmail.com>
2017-10-01 21:38:05 +0300

    Temp: 404 (19.1)

---
+++ resources/views/frontend/temp/404.blade.php
@@ -0,0 +1,25 @@
+@extends('frontend.layouts.default')
+@section('title')404 - @stop
+@section('content')
+
+<div class="bg-yellow text-center">
+	<div class="v-indent-60"></div><div class="v-indent-20"></div>
+
+	<img src="/assets/images/404.png" alt="404">
+
+	<div class="v-indent-40"></div>
+
+	<h5 class="grey3 top-20">Нам дуже прикро, але сторінка не знайдена...</h5>
+
+	<div class="v-indent-20"></div>
+</div>
+
+
+<div class="container-half text-center">
+
+	<div class="v-indent-60"></div>
+
+	<a href="#" class="button button-red f18 text-upper">Повернутися до меню страв</a>
+
+</div>
+@stop
