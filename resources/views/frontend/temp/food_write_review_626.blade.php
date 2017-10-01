f552b5e25aeca635a5b9a73316029c2b114ef66f
Evgen <eok8177@gmail.com>
2017-10-01 16:56:21 +0300

    Temp: food_write_review (6.2.5 - 6.2.6)

---
+++ resources/views/frontend/temp/food_write_review_626.blade.php
@@ -0,0 +1,30 @@
+@extends('frontend.layouts.default')
+@section('title')Adverts - @stop
+@section('content')
+
+
+<div class="bg-yellow text-center">
+	<div class="v-indent-120"></div>
+
+	<p><i class="fo fo-cook fo-large red"></i></p>
+
+	<h5 class="text-upper">Ви залишили відгук про повара</h5>
+
+	<div class="v-indent-60"></div>
+</div>
+
+
+<div class="container-half text-center">
+
+	<div class="v-indent-40"></div>
+
+	<p><a href="#" class="link-grey3 f16">Перейти на сторінку моїх відгуків <i class="fo fo-arrow-right fo-small"></i></a></p>
+	<p><a href="#" class="link-grey3 f16">Перейти на сторінку повара <i class="fo fo-arrow-right fo-small"></i></a></p>
+
+
+
+</div>
+
+
+@stop
+
