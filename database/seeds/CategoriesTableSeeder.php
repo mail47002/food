aec80ba74cac6cb6e05ae7a8578782e0d162736a
Roman K <modern.diz@gmail.com>
2017-10-04 10:49:18 +0300

    Update categories seeds

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -12,31 +13,62 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
       	DB::table('categories')->insert([
-            'name' => 'Category 1',
-            'sort_order' => '4',
-            'status' => 1,
-            'created_at' => DB::raw('CURRENT_TIMESTAMP'), // для timestamp
-        ]);
-
-        DB::table('categories')->insert([
-            'name' => 'Category 2',
-            'sort_order' => '3',
-            'status' => 1,
-            'created_at' => DB::raw('CURRENT_TIMESTAMP'), // для timestamp
-        ]);
-
-        DB::table('categories')->insert([
-            'name' => 'Category 3',
-            'sort_order' => '2',
-            'status' => 1,
-            'created_at' => DB::raw('CURRENT_TIMESTAMP'), // для timestamp
-        ]);
-
-        DB::table('categories')->insert([
-            'name' => 'Category 4',
-            'sort_order' => '1',
-            'status' => 1,
-            'created_at' => DB::raw('CURRENT_TIMESTAMP'), // для timestamp
+      	    [
+                'name'       => 'Супи',
+                'sort_order' => 0,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Другі страви',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Каші',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Десерти і торти',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Закуски',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Салати',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Випічка і пироги',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Напої',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Спеції',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Консервація',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ], [
+                'name'       => 'Алкоголь',
+                'sort_order' => 1,
+                'status'     => 1,
+                'created_at' => Carbon::now()->toDateTimeString()
+            ]
         ]);
     }
 }
