144a947511925f29b8c2bc55a745be344c39474b
Evgen <eok8177@gmail.com>
2018-01-25 21:07:54 +0200

    5.4.1 add success block

--- resources/views/frontend/temp/5_4_1.blade.php
+++ resources/views/frontend/temp/5_4_1.blade.php
@@ -162,6 +162,23 @@
 			</div>
 
 
+			<div class="wide-thumb profile-messages success">
+				<div class="left with-image">
+					<div class="title">Повар <a href="/profile/#" class="link-blue">Оксана</a> підтвердила ваше замовлення</div>
+					<div class="avatar"><div class="rounded"><img src="http://lorempixel.com/50/50/" alt="foto"></div></div>
+					<div class="message">
+						<p><a href="#" class="link-blue">М'ясне рагу з овочами</a> </p>
+						<p><i class="fo fo-time red"></i>15 грудня (обід) <span class="price">80 грн.</span></p>
+					</div>
+
+					<div class="image hidden-xs"><img src="http://lorempixel.com/133/90/" alt=""></div>
+				</div>
+				<div class="right left-border">
+					<p class="date">10:15 2 липня 2016</p>
+				</div>
+			</div>
+
+
 		</div>
 
 
