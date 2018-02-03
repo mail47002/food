0d809eb77709892d6ecca1320d63882d193de289
antarx <antarx_r@mail.ru>
2018-02-03 14:57:48 +0200

    Update notifications

--- resources/views/frontend/includes/notification.blade.php
+++ resources/views/frontend/includes/notification.blade.php
@@ -13,6 +13,21 @@
     </li>
 @endif
 
+@if($notification->type === 'App\Notifications\OrderConfirmed')
+    <li class="top-message ">
+        <a href="#"> {{-- Ссылка на сообщение ? --}}
+            <div class="avatar">
+                <div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div>
+            </div>
+            <div class="message">
+                <strong><object>Повар <a href="/account/#" class="link-blue">{{ $notification->data['advert']['user']['name'] }}</a> підтвердила ваше замовлення</object></strong>
+                <p>В принципе вкусно,если сделать для одного ....</p>
+                <p class="date">{{ Date::parse($notification->created_at)->format('H:i d F Y') }}</p>
+            </div>
+        </a>
+    </li>
+@endif
+
 @if($notification->type === 'App\Notifications\OrderCanceled')
     <li class="top-message order">
         <a href="#"> {{-- Ссылка на сообщение ? --}}
@@ -41,6 +56,21 @@
     </li>
 @endif
 
+@if($notification->type === 'App\Notifications\AdvertDeleted')
+    <li class="top-message deleted">
+        <a href="#"> {{-- Ссылка на сообщение ? --}}
+            <div class="avatar">
+                <div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div>
+            </div>
+            <div class="message">
+                <strong><object>Ваше оголошення видалено</object></strong>
+                <p>В принципе вкусно,если сделать для одного ....</p>
+                <p class="date">{{ Date::parse($notification->created_at)->format('H:i d F Y') }}</p>
+            </div>
+        </a>
+    </li>
+@endif
+
 {{--<li class="top-message email">--}}{{-- менять цвет --}}
     {{--<a href="#">--}}{{-- Ссылка на сообщение ? --}}
         {{--<div class="avatar"><div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div></div>--}}
@@ -53,23 +83,11 @@
     {{--</a>--}}
 {{--</li>--}}
 
-
 {{--<li class="top-message review">--}}
     {{--<a href="#">--}}{{-- Ссылка на сообщение ? --}}
         {{--<div class="avatar"><div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div></div>--}}
         {{--<div class="message">--}}
             {{--<strong><object>Повар <a href="/account/#" class="link-blue">Оксана</a> відповіла на ваш відгук про страву</object></strong>--}}
-            {{--<p>В принципе вкусно,если сделать для одного ....</p>--}}
-            {{--<p class="date">10:15 2 липня 2016</p>--}}
-        {{--</div>--}}
-    {{--</a>--}}
-{{--</li>--}}
-
-{{--<li class="top-message deleted">--}}
-    {{--<a href="#">--}}{{-- Ссылка на сообщение ? --}}
-        {{--<div class="avatar"><div class="rounded"><img src="/uploads/avatar.jpg" alt="foto"></div></div>--}}
-        {{--<div class="message">--}}
-            {{--<strong><object>Ваше оголошення видалено</object></strong>--}}
             {{--<p>В принципе вкусно,если сделать для одного ....</p>--}}
             {{--<p class="date">10:15 2 липня 2016</p>--}}
         {{--</div>--}}
