91870de34aef03f8243a26f4ebb8c90b37d8af63
Evgen <eok8177@gmail.com>
2018-02-16 23:23:21 +0200

    adverts: address

--- resources/views/frontend/adverts/address.blade.php
+++ resources/views/frontend/adverts/address.blade.php
@@ -6,29 +6,57 @@
                 <a href="#" type="button" class="close link-red" data-dismiss="modal"><i class="fo fo-delete"></i></a>
                 <h4 class="modal-title">Змінити регіон</h4>
             </div>
-            <div class="modal-body">
-                <p><input type="checkbox" id="ukraine" checked="checked"><label for="ukraine">Уся Україна</label></p>
+            <div class="modal-body contact">
+                <p style="display: inline-block;"><input id="ukraine" type="checkbox"><label for="ukraine">Вся Україна</label></p>
+
                 <p>Населений пункт</p>
-                <p>
-                    <select class="address full-width">
-                        @foreach($cities as $city)
-                            <option value="{{ $city->id }}">{{ $city->name }}</option>
-                        @endforeach
-                    </select>
-                </p>
-                <div class="row">
-                    <div class="col-md-7">
+                <div class="marker">
+                    <input id="city" type="text" placeholder="" class="wide">
+                </div>
+                <input id="city_id" name="city_id" type="hidden" value="">
+
+                <div class="row text-left">
+                    <div class="col-md-offset-1 col-md-6">
                         <p>Вулиця</p>
-                        <input class="" type="text" placeholder="Соборна">
+                        <input id="street" class="" type="text" placeholder="Соборна">
                     </div>
-                    <div class="col-md-5">
+                    <div class="col-md-3">
                         <p>№ будинку</p>
-                        <input class="" type="text" placeholder="30">
+                        <input id="number" class="" type="text" placeholder="30">
                     </div>
                 </div>
                 <p></p>
-                <a href="#" class="button button-red">Застосувати</a>
+                <a href="#" class="button button-red button-big">Застосувати</a>
             </div>
         </div>
     </div>
 </div>
+
+@push('scripts')
+{{-- Autocomplete --}}
+    <script>
+    $(function() {
+        var cities = [
+            @foreach($cities as $city)
+            {id:"{{ $city->id }}", value:"{{ $city->name }}"},
+            @endforeach
+        ];
+
+        $( "#city" ).autocomplete({
+          minLength: 0,
+          source: cities,
+          focus: function( event, ui ) {
+            $( "#city" ).val( ui.item.value );
+            $( "#city_id" ).val( ui.item.id );
+            return false;
+          },
+          select: function( event, ui ) {
+            $( "#city" ).val( ui.item.value );
+            $( "#city_id" ).val( ui.item.id );
+            return false;
+          }
+        });
+    });
+    </script>
+
+@endpush
