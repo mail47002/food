4cc4f502f6dd48800b8db9b58947258ae7087e2b
Evgen <eok8177@gmail.com>
2018-04-22 21:18:48 +0300

    account user: modal autohide

--- resources/views/frontend/account/users/success.blade.php
+++ resources/views/frontend/account/users/success.blade.php
@@ -1,5 +1,5 @@
 <div id="modal_account-update" class="modal fade" role="dialog">
-    <div class="modal-dialog">
+    <div class="modal-dialog w-710" style="height: 220px;">
         <div class="modal-content text-center">
             <div class="moda-header">
                 <a href="#" type="button" class="close link-red" data-dismiss="modal"><i class="fo fo-delete"></i></a>
@@ -11,3 +11,17 @@
         </div>
     </div>
 </div>
+
+@push('scripts')
+<script>
+$(function(){
+    $('#modal_account-update').on('show.bs.modal', function(){
+        var modalWindow = $(this);
+        clearTimeout(modalWindow.data('hideInterval'));
+        modalWindow.data('hideInterval', setTimeout(function(){
+            modalWindow.modal('hide');
+        }, 2000));
+    });
+});
+</script>
+@endpush
